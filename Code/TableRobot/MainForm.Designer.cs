d012e814451b8fd78a612298b01e80b40f0441d2
wcss <wcss2020@gmail.com>
2019-09-02 13:15:44 +0800

    ...

--- Code/TableRobot/MainForm.Designer.cs
+++ Code/TableRobot/MainForm.Designer.cs
@@ -28,22 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(633, 390);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(633, 54);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(633, 444);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "填表机器人";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Code/TableRobot/MainForm.cs
+++ Code/TableRobot/MainForm.cs
@@ -14,5 +14,70 @@ namespace TableRobot
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Win32.POINT point;
+            Win32.User.GetCursorPos(out point);
+
+            int hwnd = Win32.User.WindowFromPoint(point.x, point.y);
+
+            KeyValuePair<string, string> ctrlClassAndTextObj = GetClassAndText(new IntPtr(hwnd));
+
+            if (richTextBox1.Lines != null && richTextBox1.Lines.Length >= 16)
+            {
+                richTextBox1.Clear();
+            }
+
+            int window = Win32.User.GetParent(new IntPtr(hwnd));
+
+            int newHwnd = Win32.User.FindWindow(new StringBuilder(ctrlClassAndTextObj.Key), new StringBuilder(ctrlClassAndTextObj.Value));
+
+            KeyValuePair<string, string> windowClassAndTextObj = GetClassAndText(new IntPtr(window));
+
+            richTextBox1.AppendText("-----------------------------\n");
+            richTextBox1.AppendText("ControlHandle:" + hwnd + "\n");
+            richTextBox1.AppendText("ControlClass:" + ctrlClassAndTextObj.Key + "\n");
+            richTextBox1.AppendText("ControlText:" + ctrlClassAndTextObj.Value + "\n");
+
+            richTextBox1.AppendText("WindowClass:" + windowClassAndTextObj.Key + "\n");
+            richTextBox1.AppendText("WindowText:" + windowClassAndTextObj.Value + "\n");
+
+            richTextBox1.AppendText("-----------------------------\n");
+        }
+
+        public KeyValuePair<string, string> GetClassAndText(IntPtr hwnd)
+        {
+            KeyValuePair<string, string> result;
+            StringBuilder ctrlClassStr = new StringBuilder(2000);
+            StringBuilder ctrlNameStr = new StringBuilder(2000);
+            Win32.User.GetClassName(hwnd, ctrlClassStr, 2000);
+            Win32.User.GetWindowText(hwnd, ctrlNameStr, 2000);
+
+            result = new KeyValuePair<string, string>(ctrlClassStr.ToString().Trim(), ctrlNameStr.ToString().Trim());
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 返回写字板主窗口句柄
+            IntPtr hWnd = new IntPtr(Win32.User.FindWindow(new StringBuilder("CalcFrame"), new StringBuilder("计算器")));
+            if (!hWnd.Equals(IntPtr.Zero))
+            {
+                //返回写字板编辑窗口句柄
+                IntPtr edithWnd = new IntPtr(Win32.User.FindWindowEx(hWnd, IntPtr.Zero, null, new StringBuilder("9")));
+                if (!edithWnd.Equals(IntPtr.Zero))
+                {
+                    // 发送WM_SETTEXT 消息： "Hello World!"
+                    //SendMessage(edithWnd, WM_SETTEXT, IntPtr.Zero, new StringBuilder("Hello World!"));
+                    Text = "1111111111";
+                }
+            }
+
+            int hwnd = Win32.User.FindWindow(new StringBuilder(""), new StringBuilder());
+            Message msg = Message.Create(new IntPtr(hwnd), 0x00F5, new IntPtr(0), new IntPtr(0));
+            //点击hwnd_button句柄对应的按钮
+            Win32.User.PostMessage(msg.HWnd, msg.Msg, msg.WParam.ToInt32(), msg.LParam.ToInt32());
+        }
     }
 }
